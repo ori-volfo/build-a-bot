b5cd1f8dbc390d943ac04600b9cdbaebab78bb8a
oriv <oriv@aspireglobal.com>
2019-05-02 16:32:47 +0300

    v-if v-for

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,7 +1,12 @@
 <template>
-    <div>
+    <div class="content">
+        <button class="add-to-cart" @click="addToCart()">Add To Cart</button>
         <div class="top-row">
             <div class="top part">
+                <div class="robot-name">
+                    {{selectedRobot.head.title}}
+                    <span v-if="selectedRobot.head.onSale" class="sale">Sale!</span>
+                </div>
                 <img :src="selectedRobot.head.src" title="head"/>
                 <button @click="selectPrevHead()" class="prev-selector">&#9668;</button>
                 <button @click="selectNextHead()" class="next-selector">&#9658;</button>
@@ -31,6 +36,23 @@
                 <button @click="selectNextBase()" class="next-selector">&#9658;</button>
             </div>
         </div>
+        <div>
+            <h1>Cart</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Robot</th>
+                        <th class="cost">Cost</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(robot,index) in cart" :key="index">
+                        <td>{{robot.head.title}}</td>
+                        <td class="cost">{{robot.cost}}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
 
@@ -52,6 +74,7 @@
         data(){
             return {
                 availableParts,
+                cart: [],
                 selectedHeadIndex: 0,
                 selectedLeftArmIndex: 0,
                 selectedRightArmIndex: 0,
@@ -71,6 +94,11 @@
             }
         },
         methods:{
+            addToCart(){
+                const robot = this.selectedRobot;
+                const cost = robot.head.cost + robot.leftArm.cost + robot.rightArm.cost + robot.torso.cost + robot.base.cost;
+                this.cart.push(Object.assign({},robot,{cost}));
+            },
             selectNextHead(){
                 this.selectedHeadIndex = getNextValidIndex(this.selectedHeadIndex, availableParts.heads.length);
             },
@@ -193,5 +221,32 @@
     }
     .right .next-selector {
         right: -3px;
+    }
+    .robot-name{
+        position: absolute;
+        top:-25px;
+        text-align: center;
+        width: 100%;
+    }
+    .sale{
+        color: red;
+    }
+    .content{
+        position: relative;
+    }
+    .add-to-cart{
+        position: absolute;
+        right: 30px;
+        width: 220px;
+        padding: 3px;
+        font-size: 16px;
+    }
+    td, th{
+        text-align: left;
+        padding: 5px;
+        padding-right: 20px;
+    }
+    .cost{
+        text-align: right;
     }
 </style>
