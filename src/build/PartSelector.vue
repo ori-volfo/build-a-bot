b1cae34dd005b7857e22e16af2975efdacfaeb0a
oriv <oriv@aspireglobal.com>
2019-05-06 14:24:19 +0300

    Inter-Components
    Using Props to Share Data with Child Components

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="part">
+    <div class="part" :class="position">
         <img :src="selectedPart.src" title="arm"/>
         <button @click="selectPreviousPart()" class="prev-selector"></button>
         <button @click="selectNextPart()" class="next-selector"></button>
@@ -8,9 +8,6 @@
 </template>
 
 <script>
-    import availableParts from '../data/parts';
-
-    const parts = availableParts.heads;
 
     function getPreviousValidIndex(index, length) {
         const deprecatedIndex = index - 1;
@@ -23,18 +20,19 @@
     }
 
     export default {
+        props: ['parts', 'position'],
         data() {
             return { selectedPartIndex: 0 };
         },
         computed: {
-            selectedPart() { return parts[this.selectedPartIndex];},
+            selectedPart() { return this.parts[this.selectedPartIndex];},
         },
         methods: {
             selectNextPart() {
-                this.selectedPartIndex = getNextValidIndex(this.selectedPartIndex,parts.length);
+                this.selectedPartIndex = getNextValidIndex(this.selectedPartIndex, this.parts.length);
             },
             selectPreviousPart() {
-                this.selectedPartIndex = getPreviousValidIndex(this.selectedPartIndex,parts.length);
+                this.selectedPartIndex = getPreviousValidIndex(this.selectedPartIndex, this.parts.length);
             },
 
         },
