2b2a76005f7aeed1c5400aec90a842467e629ee3
oriv <oriv@aspireglobal.com>
2019-05-07 13:49:02 +0300

    Routing from Page to Page
    Passing Params as Props

--- src/parts/PartsInfo.vue
+++ src/parts/PartsInfo.vue
@@ -11,9 +11,16 @@
     import parts from '../data/parts';
     export default {
         name: "PartsInfo",
+        props: {
+            partType: {type : String},
+            id: {
+                type: [Number, String],
+                validator(value){ return Number.isInteger((Number(value)))}
+            }
+        },
         computed:{
             part(){
-                const { partType, id } = this.$route.params;
+                const { partType, id } = this;
                 return parts[partType].find(part => part.id === +id);
             }
         },
