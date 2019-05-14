4fe4ee9efebd30c53c2e137f48e6f9a696f1a9b3
oriv <oriv@aspireglobal.com>
2019-05-14 15:51:46 +0300

    Creating Custom Directives and Filters
    Using Directive Lifecycle Hooks
    part 2 - shorthand syntax

--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,13 +1,4 @@
-export  default {
-    bind: (element, binding) => {
-        applyStyle(element, binding);
-    },
-    update: (element, binding) => {
-        applyStyle(element, binding);
-    }
-};
-
-function applyStyle(element, binding){
+export  default function(element, binding){
     Object.keys(binding.value).forEach((position)=>{ element.style[position] = binding.value[position]});
     element.style.position = 'absolute';
 }
