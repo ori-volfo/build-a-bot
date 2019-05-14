b463792b90a91b989b144b9bf68669cf8373d6a6
oriv <oriv@aspireglobal.com>
2019-05-14 15:50:09 +0300

    Creating Custom Directives and Filters
    Using Directive Lifecycle Hooks
    part 1

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="part" :class="position">
+        {{pinPadding}}
         <img @click="showPartInfo()" :src="selectedPart.src" title="arm"/>
         <button @click="selectPreviousPart()" class="prev-selector"></button>
         <button @click="selectNextPart()" class="next-selector"></button>
-        <span v-pin="{bottom: '5px', right: '15px'}" class="sale" v-show="selectedPart.onSale">Sale!</span>
+        <span @click="pinPadding = '30px'" v-pin="{bottom: pinPadding, right: pinPadding}" class="sale" v-show="selectedPart.onSale">Sale!</span>
     </div>
 </template>
 
@@ -34,7 +35,7 @@
             }
         },
         data() {
-            return { selectedPartIndex: 0 };
+            return { selectedPartIndex: 0, pinPadding: '10px' };
         },
         computed: {
             selectedPart() { return this.parts[this.selectedPartIndex];},
--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,8 +1,13 @@
 export  default {
     bind: (element, binding) => {
-
-        Object.keys(binding.value).forEach((position)=>{ element.style[position] = binding.value[position]});
-        element.style.position = 'absolute';
-
+        applyStyle(element, binding);
+    },
+    update: (element, binding) => {
+        applyStyle(element, binding);
     }
 };
+
+function applyStyle(element, binding){
+    Object.keys(binding.value).forEach((position)=>{ element.style[position] = binding.value[position]});
+    element.style.position = 'absolute';
+}
